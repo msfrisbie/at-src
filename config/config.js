2f86b87b96e5b71fc8463a15db919525e93ec5da
Matt Frisbie <msfrisbie@gmail.com>
2017-05-05 13:01:11 -0700

    config fix

---
+++ config/config.js
@@ -0,0 +1,29 @@
+/*
+https://github.com/sequelize/cli/issues/91
+https://github.com/sequelize/cli/issues/77#issuecomment-68299495
+
+sequlize migration:create
+
+sequelize db:migrate
+
+sequelize -c my_config.js
+*/
+
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    username: process.env.POSTGRES_USERNAME,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DATABASE,
+    host: process.env.POSTGRES_HOST,
+    dialect: process.env.POSTGRES_DIALECT
+  },
+  production: {
+    username: process.env.POSTGRES_USERNAME,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DATABASE,
+    host: process.env.POSTGRES_HOST,
+    dialect: process.env.POSTGRES_DIALECT
+  }
+}
